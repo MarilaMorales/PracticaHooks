f64e51136ca52f56c2bd1d082ac897780494114c
MarilaMorales <mmorales@fwdcostarica.com>
2024-09-04 09:32:35 -0600

    Carpetas Listas

---
+++ practicaHooks/src/Routes/Routing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "../Pages/Login.jsx"
+import Registro from "../Pages/Registro.jsx"
+
+
+const Routing = () => {
+  return (
+    <Router>
+     <Routes>
+       <Route path="/Login" element={<Login />} />
+       <Route path="/Registro" element={<Registro />} />
+     </Routes>
+    </Router>
+   );
+};
+export default Routing;
